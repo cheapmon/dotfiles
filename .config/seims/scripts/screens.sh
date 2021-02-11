a828b6e6a8d42b02d5a79497c993da694e90d8d4
Simon Kaleschke <simon.kaleschke@inaudito.de>
2021-02-11 07:47:18 +0100

    [scripts] Handle Escape key

--- .config/seims/scripts/screens.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-config=$(autorandr | cut -d"(" -f1 | rofi -dmenu -i -p config)
-$(autorandr $config)
-sh $HOME/.fehbg
-sh $HOME/.config/seims/scripts/polybar.sh
-echo "Screen configuration changed to $config"
