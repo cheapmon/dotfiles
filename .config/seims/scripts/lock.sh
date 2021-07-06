6cd41693105f01c1016ddc4faaf36ea415f69c0a
Simon Kaleschke <simon.kaleschke@inaudito.de>
2021-07-06 15:50:40 +0200

    [scripts] Repair lock screen for newest version of i3lock-color

--- .config/seims/scripts/lock.sh
+++ .config/seims/scripts/lock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -
-i3lock-fancy-rapid 10 10 --insidecolor=0000001c --ringcolor=0000003e \
-  --linecolor=00000000 --keyhlcolor=ffffff80 --ringvercolor=ffffff00 \
-  --separatorcolor=22222260 --insidevercolor=ffffff1c \
-  --ringwrongcolor=ffffff55 --insidewrongcolor=ffffff1c \
-  --verifcolor=ffffff00 --wrongcolor=ff000000 --timecolor=ffffff00 \
-  --datecolor=ffffff00 --layoutcolor=ffffff00
+i3lock-fancy-rapid 10 10 --inside-color=0000001c --ring-color=0000003e \
+  --line-color=00000000 --keyhl-color=ffffff80 --ringver-color=ffffff00 \
+  --separator-color=22222260 --insidever-color=ffffff1c \
+  --ringwrong-color=ffffff55 --insidewrong-color=ffffff1c \
+  --verif-color=ffffff00 --wrong-color=ff000000 --time-color=ffffff00 \
+  --date-color=ffffff00 --layout-color=ffffff00
