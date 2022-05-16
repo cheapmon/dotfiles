864e1120358da809c9c15ab04a072aab0c288022
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2022-05-16 10:12:34 +0200

    [misc] Generate commands to allow overriding certain files

--- .config/seims/scripts/dotfiles.sh
+++ .config/seims/scripts/dotfiles.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env zsh
+if [[ $1 == "-g" ]]; then
+  FILES=$(fd -tf . $HOME/.dots | awk "{ gsub(\"$HOME/.dots\", \"\"); print }" | xargs)
+  echo "sudo groupadd dots"
+  echo "sudo usermod -aG dots $USER"
+  echo "sudo chgrp dots $FILES"
+  echo "sudo chmod g+rw $FILES"
+  exit
+fi
+
 APP_NAME="dotfiles"
 
 source $HOME/.zshrc
