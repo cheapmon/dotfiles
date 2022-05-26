c3e6f7d59df97aa53734b94e1654706e2b229337
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2022-05-26 20:25:25 +0200

    [xmonad] Baseline configuration

--- .config/seims/scripts/polybar.sh
+++ .config/seims/scripts/polybar.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Setup modules based on WM
+export MODULES_MAIN="round-left $DESKTOP_SESSION round-right margin round-left xwindow round-right margin round-left spotify round-right"
+export MODULES_RIGHT="round-left $DESKTOP_SESSION round-right margin round-left xwindow round-right"
+
 # Launch bars
 cd $HOME/.config/polybar
 if [ -n "$MONITOR" ]; then
---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,125 @@
+import Data.Map hiding (map, keys)
+import Data.Monoid
+import System.Exit
+import XMonad
+import XMonad.Actions.Warp
+import XMonad.Config.Desktop
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.IndependentScreens
+import XMonad.StackSet hiding (workspaces)
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
+
+myTerminal          = "alacritty"
+myFocusFollowsMouse = True
+myClickJustFocuses  = False
+myBorderWidth       = 2
+myModMask           = mod4Mask
+
+myWorkspaces nScreens = withScreens nScreens (map show [1..5])
+
+myNormalBorderColor  = "#000000"
+myFocusedBorderColor = "#ffffff"
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = fromList $
+  -- switch screen / move window to screen
+  [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    | (key, sc) <- zip [xK_m, xK_comma, xK_period] [0..]
+    , (f, m) <- [(view, 0), (shift, shiftMask)]]
+
+myAdditionalKeys =
+  [
+    ("M-<Return>", spawn myTerminal),
+    ("M-d",        spawn "rofi -show run"),
+
+    -- XMonad
+    ("M-q",        kill),
+    ("M-r",        spawn "xmonad --recompile; xmonad --restart"),
+    ("M-e",        io exitSuccess),
+
+    -- Layouts
+    ("M-<Space>",  sendMessage NextLayout),
+    ("M-<R>",      windows focusDown),
+    ("M-<L>",      windows focusUp),
+    ("M-<D>",      windows focusMaster),
+    ("M-<U>",      windows swapMaster),
+    ("M-j",        sendMessage Expand),
+    ("M-k",        sendMessage Shrink),
+    ("M-t",        withFocused $ windows . sink),
+    ("M-u",        sendMessage (IncMasterN 1)),
+    ("M-i",        sendMessage (IncMasterN (-1))),
+
+    -- Move to workspace
+    ("M-1",        switchTo 0 "1"),
+    ("M-2",        switchTo 0 "2"),
+    ("M-3",        switchTo 0 "3"),
+    ("M-4",        switchTo 0 "4"),
+    ("M-5",        switchTo 0 "5"),
+    ("M-6",        switchTo 1 "1"),
+    ("M-7",        switchTo 1 "2"),
+    ("M-8",        switchTo 1 "3"),
+    ("M-9",        switchTo 1 "4"),
+    ("M-0",        switchTo 1 "5"),
+
+    -- Move window to workspace
+    ("M-S-1",      shiftTo 0 "1"),
+    ("M-S-2",      shiftTo 0 "2"),
+    ("M-S-3",      shiftTo 0 "3"),
+    ("M-S-4",      shiftTo 0 "4"),
+    ("M-S-5",      shiftTo 0 "5"),
+    ("M-S-6",      shiftTo 1 "1"),
+    ("M-S-7",      shiftTo 1 "2"),
+    ("M-S-8",      shiftTo 1 "3"),
+    ("M-S-9",      shiftTo 1 "4"),
+    ("M-S-0",      shiftTo 1 "5")
+  ]
+
+myLayout = avoidStruts $ tiled ||| Mirror tiled ||| Full
+  where
+    tiled = Tall nmaster delta ratio
+    nmaster = 1
+    ratio = 1 / 2
+    delta = 3 / 100
+
+myManageHook = composeAll []
+myEventHook = mempty
+myLogHook = ewmhDesktopsLogHook
+myStartupHook = do
+  spawn "$HOME/.config/seims/scripts/polybar.sh"
+  spawnOnce "picom -b"
+
+main = do
+  nScreens <- countScreens
+  xmonad $ docks . ewmh $ desktopConfig {
+    -- simple stuff
+    terminal = myTerminal,
+    focusFollowsMouse = myFocusFollowsMouse,
+    clickJustFocuses = myClickJustFocuses,
+    borderWidth = myBorderWidth,
+    modMask = myModMask,
+    workspaces = myWorkspaces nScreens,
+    normalBorderColor = myNormalBorderColor,
+    focusedBorderColor = myFocusedBorderColor,
+    keys = myKeys,
+    -- hooks, layouts
+    layoutHook = myLayout,
+    manageHook = myManageHook,
+    handleEventHook = myEventHook,
+    logHook = myLogHook,
+    startupHook = myStartupHook
+  } `additionalKeysP` myAdditionalKeys
+
+-- Helper functions
+openScreen :: ScreenId -> X ()
+openScreen s = withScreen s view
+
+switchTo :: ScreenId -> WorkspaceId -> X ()
+switchTo s w = sequence_ [openScreen s, windows $ onCurrentScreen view w, warpToWindow 0.5 0.5]
+
+shiftTo :: ScreenId -> WorkspaceId -> X ()
+shiftTo s w = sequence_ [windows $ shift (marshall s w), switchTo s w]
+
+withScreen :: ScreenId -> (WorkspaceId -> WindowSet -> WindowSet) -> X ()
+withScreen s f = screenWorkspace s >>= flip whenJust (windows . f)
