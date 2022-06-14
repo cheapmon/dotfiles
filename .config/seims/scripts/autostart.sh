e317862293a1a530f09a53ad38c6b6641c10d4b8
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2022-06-14 15:33:34 +0200

    [misc] Only run autostart between 9 and 18

--- .config/seims/scripts/autostart.sh
+++ .config/seims/scripts/autostart.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$WORK" == "yes" ]; then
+HOUR=$(($(date +%H)))
+
+if [ "$WORK" == "yes" -a $HOUR -ge 8 -a $HOUR -lt 18 ]; then
   firefox-developer-edition &
   thunderbird &
   slack &
