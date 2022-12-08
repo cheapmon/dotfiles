100e9621d60dd327e41b08a0c62a9d210e9a8b03
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2022-12-08 10:55:31 +0100

    [i3] More versatile layout script

--- .config/seims/scripts/layout.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-i3-msg "workspace --no-auto-back-and-forth $1"
-i3-msg "append_layout $HOME/.i3/workspace-$2.json"
-i3-msg "exec alacritty --working-directory=$HOME/git/architecture/apps/$3"
-i3-msg "exec alacritty --working-directory=$HOME/git/architecture/apps/$3"
-i3-msg "exec alacritty --working-directory=$HOME/git/architecture/apps/$3"
