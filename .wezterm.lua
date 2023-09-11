ba56c781f2c040517b2f9205b718ecb495829d75
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2023-09-11 11:07:41 +0200

    [wezterm] Try out new terminal emulator

---
+++ .wezterm.lua
@@ -0,0 +1,23 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.window_background_opacity = 0.8
+config.color_scheme = 'Catppuccin Macchiato'
+config.enable_tab_bar = false
+config.font = wezterm.font 'IosevkaTerm Nerd Font'
+config.font_size = 17.0
+config.window_decorations = "TITLE"
+config.enable_scroll_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+config.scrollback_lines = 100000
+
+return config
