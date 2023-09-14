795b5664838185d187b35689617cc0ac0de8eea0
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2023-09-15 00:40:59 +0200

    [nvim] Install lazy.nvim if missing

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -64,7 +64,18 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- Plugins
-vim.opt.rtp:prepend(vim.fn.stdpath("data") .. "/lazy/lazy.nvim")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   "catppuccin/nvim",
