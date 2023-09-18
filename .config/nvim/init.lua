8f63b7187b20ae27860c7a6572b5f1ac56fc126a
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2023-09-18 11:00:19 +0200

    [nvim] Improve > and <

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,6 +55,11 @@ end)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<", "<<")
+vim.keymap.set("n", ">", ">>")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
