dc12b273e58f55bc78fbfbaa150268a8c2ae1abe
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2023-09-22 15:19:18 +0200

    [nvim] Add completion key bindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -215,6 +215,11 @@ lsp_zero.on_attach(function(_, bufnr)
 end)
 
 cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping.select_next_item(),
+    ["<S-Tab>"] = cmp.mapping.select_prev_item()
+  }),
   sources = {
     { name = "nvim_lsp" },
     { name = "buffer" }
