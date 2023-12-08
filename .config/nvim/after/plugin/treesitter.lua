394afac9bb3697fd8cfc8609379e53fa3e781b01
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2023-12-08 15:07:53 +0100

    [nvim] Enable folding

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -5,3 +5,7 @@ treesitter.setup({
   auto_install = true,
   highlight = { enable = true }
 })
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.cmd("autocmd BufReadPost,FileReadPost * normal zR")
