53bc7e8df74f730e637145e6d8d542e945cd2261
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2024-01-18 17:06:37 +0100

    [nvim] Update plugins

--- .config/nvim/lua/seims/lazy.lua
+++ .config/nvim/lua/seims/lazy.lua
@@ -15,7 +15,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   { "catppuccin/nvim" },
-  { "nvim-telescope/telescope.nvim", tag = "0.1.3", dependencies = { "nvim-lua/plenary.nvim" } },
+  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   { "ThePrimeagen/harpoon", dependencies = { "nvim-lua/plenary.nvim" } },
   { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
