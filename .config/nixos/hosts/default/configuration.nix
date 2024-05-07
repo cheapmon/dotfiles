4bf4b19d18373bc355c8bf9bcceec0016a13367d
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2024-05-07 23:07:11 +0200

    [nix] Setup lots of packages

--- .config/nixos/hosts/default/configuration.nix
+++ .config/nixos/hosts/default/configuration.nix
@@ -126,12 +126,13 @@
     ripgrep
     magic-wormhole
     fastfetch
-
     feh
     wezterm
     rofi
+    gnome.eog
     gnome.nautilus
     gnome.gnome-themes-extra
+    gnome.simple-scan
     paper-gtk-theme
     paper-icon-theme
     (polybar.override {
@@ -148,6 +149,50 @@
     htop
     xorg.xkill
     networkmanagerapplet
+    pavucontrol
+    element-desktop
+    flameshot
+    clipmenu
+    thunderbird
+    redshift
+    unclutter
+    fd
+    arandr
+    autorandr
+    xdotool
+    baobab
+    bat
+    cmatrix
+    cowsay
+    lolcat
+    cups
+    curlie
+    docker
+    docker-compose
+    evince
+    gource
+    sl
+    rsync
+    solaar
+    tokei
+    vlc
+    wget
+    jq
+    yq
+    acpi
+    telegram-desktop
+    signal-desktop
+    (i3lock-fancy-rapid.overrideAttrs {
+      propagatedBuildInputs = [ i3lock-color ];
+
+      postPatch = ''
+        substituteInPlace i3lock-fancy-rapid.c \
+          --replace '"i3lock"' '"${i3lock-color}/bin/i3lock-color"'
+      '';
+    })
+    ruby
+    upower
+    glow
 
     # Neovim
     neovim
