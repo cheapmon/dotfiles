fcc3a04f7108683e85cadfabd530203aba1edd17
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2024-05-08 15:25:41 +0200

    [misc] Remove .dots folder

--- .config/seims/scripts/dotfiles.sh
+++ .config/seims/scripts/dotfiles.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env zsh
-if [[ $1 == "-g" ]]; then
-  FILES=$(fd -tf . $HOME/.dots | awk "{ gsub(\"$HOME/.dots\", \"\"); print }" | xargs)
-  echo "sudo groupadd -f dots"
-  echo "sudo usermod -aG dots $USER"
-  for FILE in $(echo "$FILES" | xargs); do
-    echo "sudo mkdir -p $(dirname $FILE)"
-  done
-  echo "sudo touch $FILES"
-  echo "sudo chgrp dots $FILES"
-  echo "sudo chmod g+rw $FILES"
-  exit
-fi
-
 APP_NAME="dotfiles"
 
 source $HOME/.zshrc
@@ -30,9 +17,3 @@ esac
 
 i3-msg restart
 dunstify -a $APP_NAME -u $URGENCY $MESSAGE
-
-for FILE in $(fd -tf . $HOME/.dots | awk "{ gsub(\"$HOME/.dots\", \"\"); print }"); do
-  ORIGIN="$HOME/.dots$FILE"
-  echo "cp $ORIGIN $FILE"
-  cp $ORIGIN $FILE
-done
