9af124b7c5b808037bbe9173174fcbe143863476
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2025-03-03 11:16:05 +0100

    [tmux] Update plugins

--- .config/nixos/hosts/t14s/configuration.nix
+++ .config/nixos/hosts/t14s/configuration.nix
@@ -26,7 +26,6 @@
     TOUCHPAD = "ELAN0676:00 04F3:3195 Touchpad";
 
     VIM_COLORSCHEME = "catppuccin-mocha";
-    TMUX_COLORSCHEME = "mocha";
     FZF_DEFAULT_OPTS = ''
       --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
       --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
