756a56a8c4598102754b83d9e6fe5c54bb1e8018
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2025-03-18 16:32:59 +0100

    [tmuxifier] Edit window

--- .tmuxifier/layouts/core-console.window.sh
+++ .tmuxifier/layouts/core-console.window.sh
@@ -1,4 +1,4 @@
 window_root "~/git/architecture/apps/core"
 
 new_window "core-console"
-run_cmd "docker-compose run --rm core rails c"
+run_cmd "docker-compose run --label traefik.enable=false --rm core rails c"
