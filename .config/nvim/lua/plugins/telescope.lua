2ee61acbe1ec811065c3510b595f66a4dbb34ae1
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2025-08-26 15:28:53 +0200

    [nvim] Cache telescope pickers

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,6 +4,10 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-fzf-native.nvim" },
     opts = {
       defaults = {
+        cache_picker = {
+          num_pickers = 10,
+          ignore_empty_prompt = true,
+        },
         extensions = {
           fzf = {
             fuzzy = true,
@@ -23,6 +27,7 @@ return {
       { "<leader>td", function() require("telescope.builtin").lsp_document_symbols() end },
       { "<leader>tr", function() require("telescope.builtin").resume() end },
       { "<leader>to", function() require("telescope.builtin").oldfiles() end },
+      { "<leader>tp", function() require("telescope.builtin").pickers() end },
     },
   },
   {
