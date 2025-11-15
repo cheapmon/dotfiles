5692d3a593cc80ddde4d951d3a35814d0f48306f
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2025-11-15 13:40:43 +0100

    [nix] Ensure vboxnet0 address waits for interface

--- .config/nixos/modules/defaults/virtualisation.nix
+++ .config/nixos/modules/defaults/virtualisation.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   virtualisation.docker = {
@@ -9,4 +10,9 @@
   };
 
   virtualisation.virtualbox.host.enable = true;
+
+  systemd.services."network-addresses-vboxnet0" = lib.mkIf (config.virtualisation.virtualbox.host.enable or false) {
+    after = ["vboxnet0.service"];
+    requires = ["vboxnet0.service"];
+  };
 }
