ff8000f6bedafb5998703c7780a16b90280e400d
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2025-11-24 22:30:33 +0100

    [nvim] Add missing file

---
+++ .config/nvim/lua/seims/lsp.lua
@@ -0,0 +1,99 @@
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+
+-- bashls
+vim.lsp.config("bashls", {
+  cmd = { "bash-language-server", "start" },
+  settings = {
+    bashIde = {
+      globPattern = vim.env.GLOB_PATTERN or "*@(.sh|.inc|.bash|.command)",
+    },
+  },
+  filetypes = { "bash", "sh" },
+  root_markers = { ".git" },
+})
+vim.lsp.enable("bashls")
+
+-- jsonls
+vim.lsp.config("jsonls", {
+  cmd = { "vscode-json-language-server", "--stdio" },
+  filetypes = { "json", "jsonc" },
+  init_options = {
+    provideFormatter = true,
+  },
+  root_markers = { ".git" },
+})
+vim.lsp.enable("jsonls")
+
+-- lua_ls
+vim.lsp.config("lua_ls", {
+  cmd = { "lua-language-server" },
+  filetypes = { "lua" },
+  root_markers = {
+    ".emmyrc.json",
+    ".luarc.json",
+    ".luarc.jsonc",
+    ".luacheckrc",
+    ".stylua.toml",
+    "stylua.toml",
+    "selene.toml",
+    "selene.yml",
+    ".git",
+  },
+  settings = {
+    Lua = {
+      codeLens = { enable = true },
+      hint = { enable = true, semicolon = "Disable" },
+    },
+  },
+})
+vim.lsp.enable("lua_ls")
+
+-- rubocop
+local nix_shell_path = vim.fn.expand("~/.config/nix/shells/ruby.nix")
+
+vim.lsp.config("rubocop", {
+  cmd = { "nix-shell", nix_shell_path, "--run", "rubocop --lsp" },
+  filetypes = { "ruby" },
+  root_markers = { "Gemfile", ".git" },
+})
+vim.lsp.enable("rubocop")
+
+-- solargraph
+local nix_shell_path = vim.fn.expand("~/.config/nix/shells/ruby.nix")
+
+vim.lsp.config("solargraph", {
+  cmd = { "nix-shell", nix_shell_path, "--run", "solargraph stdio" },
+  settings = {
+    solargraph = {
+      diagnostics = true,
+    },
+  },
+  init_options = { formatting = true },
+  filetypes = { "ruby" },
+  root_markers = { "Gemfile", ".git" },
+})
+vim.lsp.enable("solargraph")
+
+-- tinymist
+vim.lsp.config("tinymist", {
+  cmd = { "tinymist" },
+  filetypes = { "typst" },
+  root_markers = { ".git" },
+})
+vim.lsp.enable("tinymist")
+
+-- ts_ls
+vim.lsp.config("ts_ls", {
+  init_options = { hostInfo = "neovim" },
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" },
+})
+vim.lsp.enable("ts_ls")
