379cc707902d944923ea82b3bbdd08734df3a79f
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2026-04-15 14:55:34 +0200

    [nix] Update system

--- .config/nixos/modules/defaults/derivations/acli.nix
+++ .config/nixos/modules/defaults/derivations/acli.nix
@@ -6,7 +6,7 @@ stdenv.mkDerivation {
   name = "acli";
   src = fetchurl {
     url = "https://acli.atlassian.com/linux/latest/acli_linux_amd64/acli";
-    sha256 = "VXCIb18BZS0j5gv/x+ReN0pD3UALL9+E+eBQAe9LHlU=";
+    sha256 = "9JYkwS4QmaeKMEASldFpuIr+nvenZFmOmTjTnRCjLBc=";
   };
   unpackPhase = ''
     runHook preUnpack
