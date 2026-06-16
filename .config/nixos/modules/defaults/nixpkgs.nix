8140a17b44543258ead3d5a06a0adb400594b3de
Simon Kaleschke <simon.kaleschke.leipzig@gmail.com>
2026-06-16 16:44:26 +0200

    [nix] Fix appimage-run bwrap ambient caps issue

--- .config/nixos/modules/defaults/nixpkgs.nix
+++ .config/nixos/modules/defaults/nixpkgs.nix
@@ -5,4 +5,23 @@
   ...
 }: {
   nixpkgs.config.allowUnfree = true;
+
+  # Hyprland's CAP_SYS_NICE wrapper leaks ambient capabilities to all child
+  # processes. bubblewrap refuses to run when the calling process has effective
+  # capabilities but the bwrap binary itself is not setuid.
+  # We only patch buildFHSEnv (used by appimage-run, steam-run, etc.) instead
+  # of overriding bubblewrap globally, to avoid infinite recursion.
+  nixpkgs.overlays = [
+    (final: prev: {
+      buildFHSEnv = prev.buildFHSEnv.override (oldArgs: {
+        bubblewrap = prev.writeShellScriptBin "bwrap" ''
+          exec ${prev.python3}/bin/python3 -c '
+import ctypes, os, sys
+ctypes.CDLL(None).prctl(47, 4, 0, 0, 0)
+os.execv("${prev.bubblewrap}/bin/bwrap", ["bwrap"] + sys.argv[1:])
+          ' "$@"
+        '';
+      });
+    })
+  ];
 }
